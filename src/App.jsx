4d989aa2dc463a71fc20764f7854d0012efa04e4
aromalsur <aromalsureshr@gmail.com>
2024-09-09 15:13:26 +0530

    add user model

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+import Header from './components/Header'
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+
+    <BrowserRouter>
+
+      <Header/>
+
+      <Routes>
+          
+          <Route path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/signin' element={<SignIn/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+
+    </BrowserRouter>
+
+   
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FaSearch } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className='bg-slate-100'>
+
+       <div className='flex justify-between items-center bg-slate-100 mx-auto p-3 max-w-6xl'>
+       <h1 className='font-mono font-extrabold text-2xl'>
+            <span className='text-red-500'>Used</span>
+            <span className='text-green-500'>Cars</span>
+        </h1>
+
+        <div className='flex justify-center rounded-lg items-center bg-white p-1'>
+            <input type="text" placeholder='search...' 
+            className='  items-center flex p-3 focus:outline-none' />
+            <FaSearch className='text-md'/>
+            
+
+
+        </div>
+
+        <div className='sm:flex justify-between items-center gap-2 text-xs hidden '>
+
+            <Link to={'/'}>
+          <h6>Home</h6>
+          </Link>
+
+
+          <Link to={'/about'}>
+          <h6>About</h6>
+          </Link>
+
+
+          <Link to={'/profile'}>
+          <h6>Profile</h6>
+          </Link>
+        </div>
+       </div>
+
+
+
+    </header>
+  )
+}
+
+export default Header
